4067b3530149520989c13ca249ccdc906ba5bbe9
Manuel Almaguer <manunoly@gmail.com>
2017-08-21 16:35:57 -0500

    add speaker profile

--- src/pages/user-profile/user-profile.ts
+++ src/pages/user-profile/user-profile.ts
@@ -1,25 +1,28 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the UserProfilePage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { AuthProvider } from "../../providers/auth";
 
 @IonicPage()
 @Component({
-  selector: 'page-user-profile',
-  templateUrl: 'user-profile.html',
+  selector: "page-user-profile",
+  templateUrl: "user-profile.html"
 })
 export class UserProfilePage {
+  displayName = "Visitante";
+  user: any;
+  picture: string = "https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg";
+  userProfile: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private authS: AuthProvider
+  ) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad UserProfilePage');
+    this.user = this.authS.getUser();
   }
-
+  goToHomePage(){
+    this.navCtrl.push("navegarPage");
+  }
 }
